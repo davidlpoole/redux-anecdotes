e18fce627b12b56afa3b893d1be08945f6bf9748
David Poole <14233215+davidlpoole@users.noreply.github.com>
2021-09-24 14:31:39 +1200

    refactored Anecdotes + form into components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Anecdotes from './components/Anecdotes'
+import CreateAnecdote from './components/CreateAnecdote'
+
+const App = () => {
+
+  return (
+    <div>
+      <Anecdotes />
+      <CreateAnecdote />
+    </div>
+  )
+}
+
+export default App
